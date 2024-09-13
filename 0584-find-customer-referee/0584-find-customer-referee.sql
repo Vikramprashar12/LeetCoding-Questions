4a64fe5e2cd21e92db2d34dbf47e1916e0cf8e27
Vikram Prashar <102492769+Vikramprashar12@users.noreply.github.com>
2024-09-13 19:47:33 -0400

    Time: 376 ms (98.23%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+# Step 1: Understand output format, name.
+# Step 2: Filter using WHERE clause.
+
+select name from Customer where referee_id != 2 or isnull(referee_id);
